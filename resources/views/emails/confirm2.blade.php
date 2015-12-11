3febe9820feab14dc8e1efd2eeebf86dac17c6c6
Aakash Rajvanshi <aakashrajvanshi@gmail.com>
2015-12-11 05:19:58 -0500

    inline css in emails.

--- resources/views/emails/confirm2.blade.php
+++ resources/views/emails/confirm2.blade.php
@@ -1,252 +1,98 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Zen Flat Welcome Email</title>
-    <style type="text/css" media="screen">
-
-        /* Force Hotmail to display emails at full width */
-        .ExternalClass {
-            display: block !important;
-            width: 100%;
-        }
-
-        /* Force Hotmail to display normal line spacing */
-        .ExternalClass,
-        .ExternalClass p,
-        .ExternalClass span,
-        .ExternalClass font,
-        .ExternalClass td,
-        .ExternalClass div {
-            line-height: 100%;
-        }
-
-        body,
-        p,
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            margin: 0;
-            padding: 0;
-        }
-
-        body,
-        p,
-        td {
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 15px;
-            color: #333333;
-            line-height: 1.5em;
-        }
-
-        h1 {
-            font-size: 24px;
-            font-weight: normal;
-            line-height: 24px;
-        }
-
-        body,
-        p {
-            margin-bottom: 0;
-            -webkit-text-size-adjust: none;
-            -ms-text-size-adjust: none;
-        }
-
-        img {
-            outline: none;
-            text-decoration: none;
-            -ms-interpolation-mode: bicubic;
-        }
-
-        a img {
-            border: none;
-        }
-
-        .background {
-            background-color: #333333;
-        }
-
-        table.background {
-            margin: 0;
-            padding: 0;
-            width: 100% !important;
-        }
-
-        .block-img {
-            display: block;
-            line-height: 0;
-        }
-
-        a {
-            color: white;
-            text-decoration: none;
-        }
-
-        a,
-        a:link {
-            color: #2A5DB0;
-            text-decoration: underline;
-        }
-
-        table td {
-            border-collapse: collapse;
-        }
-
-        td {
-            vertical-align: top;
-            text-align: left;
-        }
-
-        .wrap {
-            width: 600px;
-        }
-
-        .wrap-cell {
-            padding-top: 30px;
-            padding-bottom: 30px;
-        }
-
-        .header-cell,
-        .body-cell,
-        .footer-cell {
-            padding-left: 20px;
-            padding-right: 20px;
-        }
-
-        .header-cell {
-            background-color: #eeeeee;
-            font-size: 24px;
-            color: #ffffff;
-        }
-
-        .body-cell {
-            background-color: #ffffff;
-            padding-top: 30px;
-            padding-bottom: 34px;
-        }
-
-        .footer-cell {
-            background-color: #eeeeee;
-            text-align: center;
-            font-size: 13px;
-            padding-top: 30px;
-            padding-bottom: 30px;
-        }
-
-        .card {
-            width: 400px;
-            margin: 0 auto;
-        }
-
-        .data-heading {
-            text-align: right;
-            padding: 10px;
-            background-color: #ffffff;
-            font-weight: bold;
-        }
-
-        .data-value {
-            text-align: left;
-            padding: 10px;
-            background-color: #ffffff;
-        }
-
-        .force-full-width {
-            width: 100% !important;
-        }
-
-    </style>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Confirm your email</title>
+    <style type="text/css" media="screen">a:link {color:#2A5DB0;text-decoration:underline}</style>
     <style type="text/css" media="only screen and (max-width: 600px)">
         @media only screen and (max-width: 600px) {
             body[class*="background"],
             table[class*="background"],
             td[class*="background"] {
-                background: #eeeeee !important;
+                background: #eeeeee;
             }
 
             table[class="card"] {
-                width: auto !important;
+                width: auto;
             }
 
             td[class="data-heading"],
             td[class="data-value"] {
-                display: block !important;
+                display: block;
             }
 
             td[class="data-heading"] {
-                text-align: left !important;
+                text-align: left;
                 padding: 10px 10px 0;
             }
 
             table[class="wrap"] {
-                width: 100% !important;
+                width: 100%;
             }
 
             td[class="wrap-cell"] {
-                padding-top: 0 !important;
-                padding-bottom: 0 !important;
+                padding-top: 0;
+                padding-bottom: 0;
             }
         }
     </style>
 </head>
 
-<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" bgcolor="" class="background">
-<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" class="background">
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" bgcolor="#333" style="-ms-text-size-adjust:none; -webkit-text-size-adjust:none; background-color:#333; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; margin:0; margin-bottom:0; padding:0">
+<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="background-color:#333; margin:0; padding:0; width:100%" bgcolor="#333">
     <tr>
-        <td align="center" valign="top" width="100%" class="background">
+        <td align="left" valign="top" width="100%" style="background-color:#333; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; text-align:left; vertical-align:top" bgcolor="#333">
             <center>
-                <table cellpadding="0" cellspacing="0" width="600" class="wrap">
+                <table cellpadding="0" cellspacing="0" width="600" style="width:600px">
                     <tr>
-                        <td valign="top" class="wrap-cell" style="padding-top:30px; padding-bottom:30px;">
-                            <table cellpadding="0" cellspacing="0" class="force-full-width">
+                        <td valign="top" style="border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; padding-bottom:30px; padding-top:30px; text-align:left; vertical-align:top" align="left">
+                            <table cellpadding="0" cellspacing="0" style="width:100%" width="100%">
                                 <tr>
-                                    <td height="60" valign="top" class="header-cell">
-                                        <img width="200" height="60" src="http://www.mrpetition.com/images/default/logo.png" alt="Mr. Petition logo">
+                                    <td height="60" valign="top" style="background-color:#eee; border-collapse:collapse; color:#fff; font-family:Arial, Helvetica, sans-serif; font-size:24px; line-height:1.5em; padding-left:20px; padding-right:20px; text-align:left; vertical-align:top" bgcolor="#eee" align="left">
+                                        <img width="200" height="60" src="http://www.mrpetition.com/images/default/logo.png" alt="Mr. Petition logo" style="-ms-interpolation-mode:bicubic; outline:none; text-decoration:none">
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td valign="top" class="body-cell">
+                                    <td valign="top" style="background-color:#fff; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; padding-bottom:34px; padding-left:20px; padding-right:20px; padding-top:30px; text-align:left; vertical-align:top" bgcolor="#fff" align="left">
 
                                         <table cellpadding="0" cellspacing="0" width="100%" bgcolor="#ffffff">
                                             <tr>
-                                                <td valign="top" style="padding-bottom:15px; background-color:#ffffff;">
-                                                    <h1>Welcome to Mr. Petition</h1>
+                                                <td valign="top" style="background-color:#fff; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; padding-bottom:15px; text-align:left; vertical-align:top" bgcolor="#fff" align="left">
+                                                    <h1 style="font-size:24px; font-weight:normal; line-height:24px; margin:0; padding:0">Welcome to Mr. Petition</h1>
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td valign="top" style="padding-bottom:20px; background-color:#ffffff;">
+                                                <td valign="top" style="background-color:#fff; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; padding-bottom:20px; text-align:left; vertical-align:top" bgcolor="#fff" align="left">
                                                     <b><br>Hey {{ $name }}, thanks for signing up!</b><br><br>
                                                     We're really excited for you to join our community! Please confirm your email id and activate your account.
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td>
+                                                <td style="border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; text-align:left; vertical-align:top" align="left" valign="top">
                                                     <table cellspacing="0" cellpadding="0" width="100%" bgcolor="#ffffff">
                                                         <tr>
-                                                            <td style="width:200px;background:#008000;">
-                                                                <div><!--[if mso]>
+                                                            <td style="background:#008000; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; text-align:left; vertical-align:top; width:200px" align="left" valign="top" width="200">
+                                                                <div>
+                                                                    <!--[if mso]>
                                                                     <v:rect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="#" style="height:40px;v-text-anchor:middle;width:200px;" stroke="f" fillcolor="#008000">
                                                                         <w:anchorlock/>
                                                                         <center>
                                                                     <![endif]-->
-                                                                    <a href="http://petclown.com/confirm/{{$token}}"
-                                                                       style="background-color:#008000;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:18px;line-height:40px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;">Confirm Email</a>
+                                                                    <a href="http://petclown.com/confirm/%7B%7B%24token%7D%7D" style="-webkit-text-size-adjust:none; background-color:#008000; color:#fff; display:inline-block; font-family:sans-serif; font-size:18px; line-height:40px; text-align:center; text-decoration:none; width:200px" bgcolor="#008000" align="center" width="200">Confirm Email</a>
                                                                     <!--[if mso]>
                                                                     </center>
                                                                     </v:rect>
-                                                                    <![endif]--></div>
+                                                                    <![endif]-->
+                                                                </div>
                                                             </td>
-                                                            <td width="360" style="background-color:#ffffff; font-size:0; line-height:0;">&nbsp;</td>
+                                                            <td width="360" style="background-color:#fff; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:0; line-height:0; text-align:left; vertical-align:top" bgcolor="#fff" align="left" valign="top"> </td>
                                                         </tr>
                                                     </table>
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td style="padding-top:20px;background-color:#ffffff;">
+                                                <td style="background-color:#fff; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:15px; line-height:1.5em; padding-top:20px; text-align:left; vertical-align:top" bgcolor="#fff" align="left" valign="top">
                                                     Thank you so much,<br>
                                                     Mr. Petition
                                                 </td>
@@ -255,7 +101,7 @@
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td valign="top" class="footer-cell">
+                                    <td valign="top" style="background-color:#eee; border-collapse:collapse; color:#333; font-family:Arial, Helvetica, sans-serif; font-size:13px; line-height:1.5em; padding-bottom:30px; padding-left:20px; padding-right:20px; padding-top:30px; text-align:center; vertical-align:top" bgcolor="#eee" align="center">
                                         Mr. Petition<br>
                                         an Insane Petitioner
                                     </td>
